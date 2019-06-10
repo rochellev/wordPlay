4ae9ae617907f3a7c896710a5dea70a7f031a345
Rochelle <rochellevroberts@gmail.com>
2019-06-10 14:35:19 -0700

    get input sentence and split each word, save into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function(event) {
   $("form#wordPlay").submit(function(event) {
-    // turn sentence into array
-    
+    // turn sentence into array, split at space
+    var input = $("#item1").val().split(' ');
+
+
 
     event.preventDefault();
 
