ef143c3c76c720d3ed9510f665009da8b213b0a6
Rochelle <rochellevroberts@gmail.com>
2019-06-10 15:59:33 -0700

    modified js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 $(document).ready(function(event) {
   $("form#wordPlay").submit(function(event) {
     // turn sentence into array, split at space
-    var input = $("#item1").val().split(' ');
+    var inputArray = $("#item1").val().split(' ');
+    var outputArray = [];
+    var combinedArray = []
 
+    inputArray.forEach(function(input){
+      if(input.length >= 3){
+        outputArray.push(input);
+      }
+
+    });
+
+    combinedArray= outputArray.join(inputArray);
+    alert(combinedArray);
+
+    //alert(outputArray.length);
+    var sorter = function(n){
+      return "<li>" + combinedArray[n] + "</li>"
+    }
 
 
     event.preventDefault();
